faa2c14b553f66387c70785de50f76f42ce31c33
Travis Miller <radezorack@gmail.com>
2024-10-14 12:45:38 -0400

    Initial Commit

---
+++ iOS Scanner/iOS_Scanner-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  iOS_Scanner-Bridging-Header.h
+//  iOS Scanner
+//
+//  Created by Travis Miller on 2024-10-14.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
---
+++ iOS ScannerTests/iOS_ScannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_ScannerTests.swift
+//  iOS ScannerTests
+//
+//  Created by Travis Miller on 2024-10-14.
+//
+
+import Testing
+@testable import iOS_Scanner
+
+struct iOS_ScannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
